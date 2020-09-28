15167f4c2ba56a81cbcdae52a83d3c25d4119548
Megasteel32 <megasteel32@gmail.com>
2020-09-28 10:22:34 -0500

    ploopy

---
+++ program1.py
@@ -0,0 +1,24 @@
+# By submitting this assignment, I agree to the following:
+#  "Aggies do not lie, cheat, or steal, or tolerate those who do"
+#  "I have not given or received any unauthorized aid on this assignment"
+#
+# Name:        Luca Maddaleni, 330001030
+# Section:     273
+# Assignment:  MOSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+# Date:        9/28/2020
+
+moss_list = [15.80, 19.60, 21.85, 33.61, 49.73, 51.27, 56.26, 63.06, 76.56, 76.57, 85.78, 90.74, 92.60, 99.71,
+100.51, 101.12, 101.25, 102.19, 104.85, 110.59, 125.92, 131.25, 136.04, 141.15, 148.54, 150.02]
+moss_list.append(162.76)
+moss_list.insert(8,71.01)
+print(len(moss_list))
+
+week1 = moss_list[0:6]
+week2 = moss_list[7:13]
+week3 = moss_list[14:20]
+week4 = moss_list[21:]
+
+print(week1)
+print(week2)
+print(week3)
+print(week4)
