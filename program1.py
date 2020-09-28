2ce93d0bfa0c089d23a7dcf4f200996f60233d68
Megasteel32 <megasteel32@gmail.com>
2020-09-28 10:30:51 -0500

    ploopy

--- program1.py
+++ program1.py
@@ -7,18 +7,27 @@
 # Assignment:  MOSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 # Date:        9/28/2020
 
+# Importing mean()
+from statistics import mean
+
+# Defining list
 moss_list = [15.80, 19.60, 21.85, 33.61, 49.73, 51.27, 56.26, 63.06, 76.56, 76.57, 85.78, 90.74, 92.60, 99.71,
 100.51, 101.12, 101.25, 102.19, 104.85, 110.59, 125.92, 131.25, 136.04, 141.15, 148.54, 150.02]
+
+# Appending and inserting values to list
 moss_list.append(162.76)
 moss_list.insert(8,71.01)
-print(len(moss_list))
 
+# Creating lists for each week
 week1 = moss_list[0:6]
 week2 = moss_list[7:13]
 week3 = moss_list[14:20]
 week4 = moss_list[21:]
 
-print(week1)
-print(week2)
-print(week3)
-print(week4)
+# Final print
+
+print("There were {} days of data collected".format(len(moss_list)))
+print("Average mass of week 1: {}g".format(round(mean(week1),2)))
+print("Average mass of week 2: {}g".format(round(mean(week2),2)))
+print("Average mass of week 3: {}g".format(round(mean(week3),2)))
+print("Average mass of week 4: {}g".format(round(mean(week4),2)))
